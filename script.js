6951b93fbcf072ea916f6daa423948b5ac704c44
brainfilltech <brainfilltech@gmail.com>
2023-09-08 17:46:01 -0700

    Bug fixing

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// const date = new Date("2023-9-8T6:36:1:222");
-// const [month, day, year] = [
-//   date.getMonth(),
-//   date.getDate(),
-//   date.getFullYear(),
-// ];
-
-// const currentUTCTime = Date.UTC(
-//   year,
-//   monthIndex,
-//   day,
-//   hour,
-//   minute,
-//   second,
-//   millisecond
-// );
-
-const date = new Date("2023-9-8T16:42:1:111");
-
-// document.body.innerHTML = currentUTCTime;
-// console.log(currentUTCTime);
-
 const days = [
   "Sunday",
   "Monday",
@@ -32,10 +10,9 @@ const days = [
 
 const d = new Date();
 let day = days[d.getDay()];
-//const currentUTCDay = getDay();
-const currentUTCTime = date.getUTCMilliseconds();
+const currentUTCTime = d.getUTCMilliseconds();
 
 document.getElementById("currentDayOfTheWeek").textContent = day;
 document.getElementById("currentUTCTime").textContent = currentUTCTime;
 
-console.log(currentUTCTime, day);
+console.log(currentUTCTime, currentUTCDate, day);
