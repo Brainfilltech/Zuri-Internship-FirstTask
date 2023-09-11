63d647b9b68b72b462af9dc5c0345f5cb4f965fa
brainfilltech <brainfilltech@gmail.com>
2023-09-10 19:41:06 -0700

    Latest modification

--- script.js
+++ script.js
@@ -12,5 +12,7 @@ const d = new Date();
 let day = days[d.getDay()];
 const currentUTCTime = d.getUTCMilliseconds();
 
+document.getElementById("currentDayOfTheWeek").textContent =
+  "Current Day of the Week";
 document.getElementById("currentDayOfTheWeek").textContent = day;
 document.getElementById("currentUTCTime").textContent = currentUTCTime;
