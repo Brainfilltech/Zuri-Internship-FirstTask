a4557ef598e177cd47eb24841a194f9734f32822
brainfilltech <brainfilltech@gmail.com>
2023-09-11 22:55:31 -0700

    Create a new version of the first task

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ const days = [
 
 const d = new Date();
 let day = days[d.getDay()];
-const currentUTCTime = d.getUTCMilliseconds();
 
-document.getElementById("currentDayOfTheWeek").textContent =
-  "Current Day of the Week";
+//const currentUTCTime = d.getUTCMilliseconds();
+
+const currentUTCTime = new Date(2023, 8, 11, 14, 30, 0);
+currentUTCTime.setHours(18, 36, 0, 0);
 document.getElementById("currentDayOfTheWeek").textContent = day;
 document.getElementById("currentUTCTime").textContent = currentUTCTime;
